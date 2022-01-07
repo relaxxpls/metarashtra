50460d32c8e08fae835c5a39c3e01b0285d5e3e1
Laxman Desai <lolopaw24@gmail.com>
2022-01-08 04:51:38 +0530

    👌 IMPROVE: Add styled components

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -34,18 +34,6 @@ module.exports = {
         props: true,
       },
     ],
-    "no-restricted-imports": [
-      "error",
-      {
-        paths: [
-          {
-            name: "styled-components",
-            message: "Please import from 'styled-components/macro.'",
-          },
-        ],
-        patterns: ["!styled-components/macro"],
-      },
-    ],
     "no-unused-expressions": "warn",
     "no-unused-vars": "warn",
     "import/extensions": [
