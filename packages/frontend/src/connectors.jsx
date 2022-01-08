9d2b07bacde527480fa37d911118a72ceee60dc5
Laxman Desai <lolopaw24@gmail.com>
2022-01-09 04:21:24 +0530

    👌 IMPROVE: Add web3-react hooks and connectors

---
+++ packages/frontend/src/connectors.jsx
@@ -0,0 +1,11 @@
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: { 1: process.env.RPC_URL_1 },
+  qrcode: true,
+});
