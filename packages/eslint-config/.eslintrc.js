5976f79ae2d0f470c3890b0625bbc86914a0adf1
Laxman Desai <lolopaw24@gmail.com>
2022-01-11 05:25:38 +0530

    👌 IMPROVE: Add eslint as seperate package

---
+++ packages/eslint-config/.eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+
+  env: {
+    es2021: true,
+  },
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+
+  extends: [
+    'plugin:security/recommended',
+    // 'plugin:node/recommended',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+    'airbnb',
+    'prettier',
+  ],
+
+  plugins: ['security', 'prettier'],
+
+  ignorePatterns: ['node_modules/*'],
+
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+    'linebreak-style': 'off',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-param-reassign': 'error',
+    'no-unused-expressions': 'warn',
+    'no-unused-vars': 'warn',
+
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'off',
+      {
+        devDependencies: true,
+        optionalDependencies: true,
+        peerDependencies: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
+};
