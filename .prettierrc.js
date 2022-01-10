73998e3b2e482e08fadd47f16573ee4869616888
Laxman Desai <lolopaw24@gmail.com>
2022-01-11 02:14:35 +0530

    👌 IMPROVE: Add config files and update packages

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 125,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+};
