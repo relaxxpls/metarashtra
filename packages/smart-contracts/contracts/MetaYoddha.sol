1b15680c6cdbcc59cb357bea025eed5e004019d0
Laxman Desai <lolopaw24@gmail.com>
2022-01-15 09:13:57 +0530

    👌 IMPROVE: Add more features to contract

--- packages/smart-contracts/contracts/MetaYoddha.sol
+++ packages/smart-contracts/contracts/MetaYoddha.sol
@@ -10,33 +10,73 @@ import { Counters } from '@openzeppelin/contracts/utils/Counters.sol';
 contract MetaYoddha is Ownable, ERC721URIStorage {
   using Counters for Counters.Counter;
 
-  // ? count of Yoddha nfts in circulation (1 based)
   Counters.Counter private _yoddhaTokenIds;
-  uint256 internal fee = 0.01 ether;
+  uint256 fee;
+
   mapping(uint256 => uint256) private _yoddhaLevel;
+
+  struct Yoddha {
+    uint256 id;
+    string uri;
+    uint256 level;
+  }
 
   event YoddhaBorn(address indexed owner, uint256 id, string uri);
   event YoddhaUpgraded(uint256 indexed id, string indexed newLevel);
+  event FeeUpdate(uint256 indexed fee);
 
-  constructor() ERC721('MetaYoddha', 'YDH') {}
+  constructor() ERC721('MetaYoddha', 'YDH') {
+    // ? start yoddha token id count from 1 instead of 0
+    _yoddhaTokenIds.increment();
+    updateFee(0.01 ether);
+  }
 
   function mintYoddha(string memory tokenURI) public payable {
     require(msg.value >= fee);
 
-    // ? start count from 1 instead of 0
-    _yoddhaTokenIds.increment();
     uint256 newYoddhaTokenId = _yoddhaTokenIds.current();
-
     _mint(msg.sender, newYoddhaTokenId);
     _setTokenURI(newYoddhaTokenId, tokenURI);
-
     _yoddhaLevel[newYoddhaTokenId] = 1;
 
     emit YoddhaBorn(msg.sender, newYoddhaTokenId, tokenURI);
+    _yoddhaTokenIds.increment();
   }
 
-  // ? Only the contract creater can upgrade the level of the Yoddha
-  function levelUp(uint256 _yoddhaTokenId) external onlyOwner {
+  function getYoddhasByOwner(address _owner)
+    external
+    view
+    returns (Yoddha[] memory)
+  {
+    Yoddha[] memory result = new Yoddha[](balanceOf(_owner));
+    uint256 counter = 0;
+
+    for (uint256 tokenId = 1; tokenId <= _yoddhaTokenIds.current(); tokenId++) {
+      if (ownerOf(tokenId) == _owner) {
+        result[counter] = Yoddha({
+          id: tokenId,
+          uri: tokenURI(tokenId),
+          level: _yoddhaLevel[tokenId]
+        });
+        counter++;
+      }
+    }
+
+    return result;
+  }
+
+  // ? Functions for owner (defaults to contract deployer)
+  function updateFee(uint256 _fee) public onlyOwner {
+    fee = _fee;
+    emit FeeUpdate(_fee);
+  }
+
+  function levelUpYoddhaById(uint256 _yoddhaTokenId) external onlyOwner {
     _yoddhaLevel[_yoddhaTokenId]++;
+  }
+
+  function withdraw() external payable onlyOwner {
+    address payable _owner = payable(owner());
+    _owner.transfer(address(this).balance);
   }
 }
