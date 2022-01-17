2f90fbf93ce77a6ad27a2698d3d5c670e2435ee9
Laxman Desai <lolopaw24@gmail.com>
2022-01-17 15:08:30 +0530

    👌 IMPROVE: Add user schema

---
+++ packages/backend/src/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+
+const userSchema = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isAlpha(value)) {
+          throw new Error('Username must contain only letters');
+        }
+      },
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isBtcAddress(value)) {
+          throw new Error('Invalid account address');
+        }
+      },
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.statics.isUsernameTaken = async (username) => {
+  const user = await this.findOne({ username });
+
+  return !!user;
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
