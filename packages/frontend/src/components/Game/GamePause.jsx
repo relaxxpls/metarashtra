54378413f92a88b7274a38c947089e9c7c328734
Laxman Desai <lolopaw24@gmail.com>
2022-01-18 07:03:07 +0530

    👌 IMPROVE: Seperate out game pause component

---
+++ packages/frontend/src/components/Game/GamePause.jsx
@@ -0,0 +1,57 @@
+import { Spin } from 'antd';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+
+import { gameState } from '../../recoil/atoms';
+import Button from '../shared/Button';
+import { PageCard } from '../shared/Page';
+
+const GamePause = ({ socket }) => {
+  const router = useRouter();
+  const [status, setStatus] = useRecoilState(gameState);
+
+  const handleRejoin = () => {
+    if (socket) socket.close();
+    // handleJoin();
+    // socket.open();
+    setStatus((_status) => ({ ..._status, isDisconnected: false }));
+  };
+
+  const handleContinue = () => {
+    setStatus({ ...status, isPaused: false });
+    socket?.emit('continue');
+  };
+
+  const handleExit = () => {
+    socket?.emit('exit');
+    router.push('/');
+  };
+
+  return (
+    <PageCard style={{ position: 'absolute' }}>
+      {status.loading ? (
+        <Spin style={{ display: 'flex' }} />
+      ) : (
+        <>
+          {status.isDisconnected ? (
+            <Button type="primary" onClick={handleRejoin}>
+              Rejoin
+            </Button>
+          ) : (
+            <Button type="primary" onClick={handleContinue}>
+              Continue
+            </Button>
+          )}
+
+          <Button>Settings</Button>
+
+          <Button type="primary" danger onClick={handleExit}>
+            Exit
+          </Button>
+        </>
+      )}
+    </PageCard>
+  );
+};
+
+export default GamePause;
