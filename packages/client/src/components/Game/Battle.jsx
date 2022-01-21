cb0f228ac06ac8fb925ab910e98bcd6d35f812de
Laxman Desai <lolopaw24@gmail.com>
2022-01-21 19:34:43 +0530

    👌 IMPROVE: Add basic battle container and update deps

--- packages/client/src/components/Game/Battle.jsx
+++ packages/client/src/components/Game/Battle.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 import { battleState, profileState } from '../../recoil/atoms';
 
-const Battle = () => {
+import BattleMove from './BattleMoves';
+
+const Battle = ({ socket }) => {
   const [battle, setBattle] = useRecoilState(battleState);
   const profile = useRecoilValue(profileState);
 
@@ -12,6 +14,27 @@ const Battle = () => {
       <Title>
         {battle.opponent} v/s {profile.username}
       </Title>
+
+      <MoveContainer>
+        <BattleMove
+          title="Kick"
+          timeout={2000}
+          color="#E97461"
+          socket={socket}
+        />
+        <BattleMove
+          title="Brace"
+          timeout={1000}
+          color="#008DF0"
+          socket={socket}
+        />
+        <BattleMove
+          title="Heal"
+          timeout={5000}
+          color="#00C19A"
+          socket={socket}
+        />
+      </MoveContainer>
     </BattleContainer>
   );
 };
@@ -21,7 +44,7 @@ export default Battle;
 const BattleContainer = styled.div`
   height: 100%;
   width: 100%;
-  background: url('images/terrain/battle.png');
+  background-image: url('images/terrain/battle.png');
   background-size: 100%;
   image-rendering: pixelated;
 `;
@@ -41,4 +64,22 @@ const Title = styled.h1`
   font-weight: 400;
   background: #f5f5f5dd;
   border-radius: 8px;
+`;
+
+const MoveContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+  width: 100%;
+  background: #0006;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 1) 80%
+  );
+  padding: 0.5rem;
+  overflow: auto hidden;
 `;
